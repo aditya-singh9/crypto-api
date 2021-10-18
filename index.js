a132d126a369aaf3134b8cb02e43d2e6056a2b25
Aditya Singh <75490282+aditya0930@users.noreply.github.com>
2021-10-18 09:19:40 +0530

    added searching

--- index.js
+++ index.js
@@ -48,6 +48,94 @@ app.get("/crypto", async (req, res) => {
   }
 });
 
+app.get("/crypto/:name", async (req, res) => {
+  let searchQuery = req.params.name;
+  try {
+    const resp = await got("https://goldprice.org/cryptocurrency-price");
+    const $ = cheerio.load(resp.body);
+    var data = null;
+    $("tbody tr").each((i, element) => {
+      var title, markCap, price, circSupply;
+      $(element)
+        .find("td")
+        .each((rowNo, td) => {
+          if (rowNo === 1) {
+            title = $(td).text();
+          }
+          if (rowNo === 2) {
+            markCap = $(td).text();
+          }
+          if (rowNo === 3) {
+            price = $(td).text();
+          }
+          if (rowNo === 4) {
+            circSupply = $(td).text();
+          }
+        });
+      var finalInfo = {
+        name: title.trim(),
+        price: price.trim(),
+        marketCapital: markCap.trim(),
+        circulatingSupply: circSupply.trim(),
+      };
+      if (title.trim().toLowerCase() === searchQuery.trim().toLowerCase()) {
+        data = JSON.stringify(finalInfo);
+      }
+    });
+    if (data !== null) return res.status(200).send(data);
+    else
+      return res
+        .status(404)
+        .send(`info about ${searchQuery} does not exist in our database`);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+app.get("/crypto/:name", async (req, res) => {
+  let srchParam = req.params.name;
+  try {
+    const resp = await got("https://goldprice.org/cryptocurrency-price");
+    const $ = cheerio.load(resp.body);
+    var data = null;
+    $("tbody tr").each((i, element) => {
+      var title, markCap, price, circSupply;
+      $(element)
+        .find("td")
+        .each((rowNo, td) => {
+          if (rowNo === 1) {
+            title = $(td).text();
+          }
+          if (rowNo === 2) {
+            markCap = $(td).text();
+          }
+          if (rowNo === 3) {
+            price = $(td).text();
+          }
+          if (rowNo === 4) {
+            circSupply = $(td).text();
+          }
+        });
+      var finalInfo = {
+        name: title.trim(),
+        price: price.trim(),
+        marketCapital: markCap.trim(),
+        circulatingSupply: circSupply.trim(),
+      };
+      if (title.trim().toLowerCase() === srchParam.trim().toLowerCase()) {
+        data = JSON.stringify(finalInfo);
+      }
+    });
+    if (data !== null) return res.status(200).send(data);
+    else
+      return res
+        .status(404)
+        .send(`Details about ${srchParam} are not available in our database.`);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
 app.listen(port, () => {
   console.log(`app running on http://localhost:${port}`);
 });
